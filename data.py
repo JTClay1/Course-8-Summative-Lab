a646245d24824a33ccaa0ee763d9a1c6fc4b4eff
Joshua Clay <josh.t.clay1995@gmail.com>
2026-01-22 17:11:33 -0700

    data.py updated with inventory

---
+++ data.py
@@ -0,0 +1,28 @@
+# in-memory inventory "database"
+
+inventory = [
+    {
+        "id": 1,
+        "name": "Whole Milk",
+        "barcode": "012000001658",
+        "price": 3.49,
+        "stock": 24,
+        "details": {}
+    },
+    {
+        "id": 2,
+        "name": "Bananas",
+        "barcode": None,
+        "price": 0.59,
+        "stock": 120,
+        "details": {}
+    },
+    {
+        "id": 3,
+        "name": "Peanut Butter",
+        "barcode": "051500255872",
+        "price": 4.99,
+        "stock": 15,
+        "details": {}
+    }
+]
